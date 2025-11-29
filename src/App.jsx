aaf92cea7dbb9a916fe4efc7f19529610d547ed8
mehmoodzafarfreelancing-ai <mehmoodzafarfreelancing@gmail.com>
2025-11-29 13:58:31 +0500

    commenting code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,76 @@
 // src/App.jsx
-import { useState, useEffect } from "react";
+import { useState, useEffect, useEffectEvent } from "react";
 import "./App.css";
 
 function App() {
   // Make a box named recipes, start it empty ( [] ), and give me a button called setRecipes to fill it later.
   const [recipes, setRecipes] = useState([]);
- // Make a box named isLoading, start it with true, and allow me to turn it to false later.
+  // Make a box named isLoading, start it with true, and allow me to turn it to false later.
   const [isLoading, setIsLoading] = useState(true);
- // make a box named error set it null and fill it if something goes wrong
+  // make a box named error set it null and fill it if something goes wrong
   const [error, setError] = useState(null);
 
   // NEW: State for the search bar
- // create a box named searchQuery, start it with string (chicken) and then allow me to chane it later using setSearchQuery.
+  // create a box named searchQuery, start it with string (chicken) and then allow me to chane it later using setSearchQuery.
   const [searchQuery, setSearchQuery] = useState("chicken");
+
+  // We moved the fetch logic into a reusable function
+  // This function is used where time is needed like (fetching data)
+  const fetchRecipes = async (query) => {
+    // Set a box having button setIsLoading to true
+    setIsLoading(true);
+    // Set a box having button setError to true
+    setError(null); // Clear old errors
+
+    // Block of code where we try to do something (that might fail)
+    try {
+      // We inject the 'query' variable into the URL dynamically
+      // Wait for fetching to be finished before contuning but donot freeze anything else
+      const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+      );
+
+      // if the request send to the server is failed then show the error (Network response was not ok)
+      if (!response.ok) throw new Error("Network response was not ok");
+
+      // Wait for response.json() to be ready when javascript object will be ready then save it inside data.
+      const data = await response.json();
+
+      // The API returns 'null' if it finds nothing, so we check for that
+      // If data returns no meals then:
+      if (data.meals === null) {
+        // clear the list
+        setRecipes([]); // clear the list
+        // Then set to error (No recipes found. Try another ingredient!)
+        setError("No recipes found. Try another ingredient!");
+      } else {
+        // Show meals of data in recepies box
+        setRecipes(data.meals);
+      }
+    } catch (err) {
+      // If try{} failed then run this catch block of code. (err) is the erroe/details of what failed
+      // Write error in console in red so that it can be seen easily
+      console.error(err);
+      // Set error to (Failed to fetch recipes)
+      setError("Failed to fetch recipes.");
+    } finally {
+      // Finally means run this at last of try and catch even if something runs or fails
+      // Set isloading to false (stop the loading)
+      setIsLoading(false);
+    }
+  };
+
+  // Run the search ONCE when the app starts (defaulting to "chicken")
+  // Run this code once when component appears on the screen
+  useEffect(() => {
+    // fetch recepies named chicken
+    fetchRecipes("chicken");
+  }, []);
+
+
+
+
+
 
 
 
@@ -23,85 +81,6 @@ function App() {
 
 
   
-  // We moved the fetch logic into a reusable function
- // This function is used where time is needed like (fetching data)
-  const fetchRecipes = async (query) => {
-
-    // Set a box having button setIsLoading to true
-    setIsLoading(true);
-    // Set a box having button setError to true
-    setError(null); // Clear old errors
-
-
-
-
-
-    // Block of code where we try to do something (that might fail)
-    try {
-      // We inject the 'query' variable into the URL dynamically
-      // Wait for fetching to be finished before contuning but donot freeze anything else
-      const response = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
-      );
-
-
-
-
-
-
-
-      // if the request send to the server is failed then show the error (Network response was not ok)
-      if (!response.ok) throw new Error("Network response was not ok");
-
-      // Wait for response.json() to be ready when javascript object will be ready then save it inside data.
-      const data = await response.json();
-
-
-
-
-
-
-
-
-
-
-
-      // The API returns 'null' if it finds nothing, so we check for that
-      // If data returns no meals then:
-      if (data.meals === null) {
-        // clear the list
-        setRecipes([]); // clear the list
-        // Then set error (No recipes found. Try another ingredient!)
-        setError("No recipes found. Try another ingredient!");
-      } else {
-        // Show meals of data in recepies box
-        setRecipes(data.meals);
-      }
-
-
-
-
-
-
-
-
-
-
-
-      
-    } catch (err) {
-      console.error(err);
-      setError("Failed to fetch recipes.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  // Run the search ONCE when the app starts (defaulting to "chicken")
-  useEffect(() => {
-    fetchRecipes("chicken");
-  }, []);
-
   // NEW: Handle the form submission
   const handleSearch = (e) => {
     e.preventDefault(); // Stop the page from reloading
